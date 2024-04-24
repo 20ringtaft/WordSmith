59e6bcf7b00529e7ec3fe324515de68688bd22ae
Taft Ring <ringtaft@gmail.com>
2024-04-23 18:23:40 -0700

    working connection with chatgpt

---
+++ test2.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+import json 
+
+def read_json():
+    with open("GPT.json","r") as f:
+        config = json.load(f)
+    return config
+
+OPENAI_API_KEY = read_json().get("api_key")
+PROMPT = read_json().get("prompt")
+client = OpenAI(
+    api_key=OPENAI_API_KEY
+)
+completion = client.chat.completions.create(
+    model = "gpt-3.5-turbo",
+    messages = [
+        {"role": "system", "content": PROMPT},
+        #replace "strong" with user input
+        {"role": "user", "content": "strong"}
+    ]
+)
+
+
+# Extract and print synonyms and their sentences
+print(completion.choices[0].message)
